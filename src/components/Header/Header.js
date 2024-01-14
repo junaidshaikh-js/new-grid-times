aefc8ca418b3b0de4cd83429731320beca479858
Junaid Shaikh <sj.shaikhjunaid@gmail.com>
2024-01-14 13:15:50 +0530

    solution - exercise 1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,22 +29,38 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <MainHeaderActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </MainHeaderActionGroup>
         <Logo />
+        <SubscribeWrapper>
+          <Button>Subscribe</Button>
+          <SubLink href="/">Already a subscriber?</SubLink>
+        </SubscribeWrapper>
       </MainHeader>
     </header>
-  );
-};
+  )
+}
 
 const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
-`;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
+`
 
 const Row = styled(MaxWidthWrapper)`
   display: flex;
   justify-content: space-between;
-`;
+`
 
 const ActionGroup = styled.div`
   display: flex;
@@ -57,7 +73,33 @@ const ActionGroup = styled.div`
   svg {
     display: block;
   }
-`;
+`
+
+const MainHeaderActionGroup = styled(ActionGroup)`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+  }
+`
+
+const SubscribeWrapper = styled.div`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+    flex-direction: column;
+    justify-self: flex-end;
+    align-self: end;
+    text-align: center;
+    gap: 8px;
+  }
+`
+
+const SubLink = styled.a`
+  font-style: italic;
+  color: var(--color-gray-900);
+`
 
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
@@ -65,6 +107,17 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
-`;
+
+  @media ${QUERIES.tabletAndUp} {
+    margin-top: 48px;
+    margin-bottom: 72px;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    margin-top: 0;
+    display: grid;
+    grid-template-columns: 1fr auto 1fr;
+  }
+`
 
 export default Header;
