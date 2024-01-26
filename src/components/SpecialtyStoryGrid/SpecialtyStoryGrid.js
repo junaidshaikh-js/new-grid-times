e7e9f4ed934f034a4bf89df6e665d892d502ffb2
Junaid Shaikh <sj.shaikhjunaid@gmail.com>
2024-01-26 15:54:18 +0530

    speciality grid

--- src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
+++ src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
@@ -6,6 +6,7 @@ import { MARKET_DATA, SPORTS_STORIES } from '../../data';
 import MarketCard from '../MarketCard';
 import SectionTitle from '../SectionTitle';
 import MiniStory from '../MiniStory';
+import { COLORS, QUERIES } from '../../constants'
 
 const SpecialtyStoryGrid = () => {
   return (
@@ -20,7 +21,7 @@ const SpecialtyStoryGrid = () => {
           Markets
         </SectionTitle>
         <MarketCards>
-          {MARKET_DATA.map((data) => (
+          {MARKET_DATA.map(data => (
             <MarketCard key={data.tickerSymbol} {...data} />
           ))}
         </MarketCards>
@@ -35,26 +36,60 @@ const SpecialtyStoryGrid = () => {
           Sports
         </SectionTitle>
         <SportsStories>
-          {SPORTS_STORIES.map((data) => (
-            <MiniStory key={data.id} {...data} />
+          {SPORTS_STORIES.map(data => (
+            <SportStoryWrapper key={data.id}>
+              <MiniStory {...data} />
+            </SportStoryWrapper>
           ))}
         </SportsStories>
       </SportsSection>
     </Wrapper>
-  );
-};
+  )
+}
 
 const Wrapper = styled.div`
   display: grid;
   gap: 48px;
-`;
 
-const MarketsSection = styled.section``;
+  @media (${QUERIES.laptopAndUp}) {
+    grid-template-columns: 1fr 1fr;
+    gap: 0;
+  }
+`
 
-const MarketCards = styled.div``;
+const MarketsSection = styled.section`
+  @media (${QUERIES.laptopAndUp}) {
+    border-right: 1px solid var(--color-gray-300);
+    padding-right: 16px;
+    margin-right: 16px;
+  }
+`
 
-const SportsSection = styled.section``;
+const MarketCards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(160px, 1fr));
+  gap: 16px;
+`
 
-const SportsStories = styled.div``;
+const SportsSection = styled.section`
+  min-width: 0;
+`
+
+const SportsStories = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(160px, 1fr));
+  gap: 16px;
+
+  @media (${QUERIES.tabletAndUp}) {
+    display: flex;
+    overflow: auto;
+  }
+`
+
+const SportStoryWrapper = styled.div`
+  @media (${QUERIES.tabletAndUp}) {
+    min-width: 220px;
+  }
+`
 
 export default SpecialtyStoryGrid;
